aab2387156639f585b959bb5b937e654ced84e7f
Pazhiloi <martinsen06@gmail.com>
2023-06-23 12:14:37 +0300

    add articles page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next';
+import cls from './ArticleDetailsPage.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { memo } from 'react';
+
+interface ArticleDetailsPageProps {
+  className?: string;
+}
+
+ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
+  const {t} = useTranslation()
+  return (
+    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+      Article Details
+    </div>
+  );
+};
+
+export default memo(ArticleDetailsPage);

0d9908398cdec2409e43174aaba1114090d89dda
Pazhiloi <martinsen06@gmail.com>
2023-07-15 18:32:21 +0300

    init auth data from server

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -14,14 +14,14 @@ const ThemeProvider = (props: ThemeProviderProps) => {
         initialTheme,
         children,
     } = props;
-  const {theme: defaultTheme = Theme.LIGHT} = useJsonSettings();
+  const {theme: defaultTheme} = useJsonSettings();
   const [isThemeInited, setThemeInited] = useState(false)
 
 
-    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme || Theme.LIGHT);
 
     useEffect(() => {
-      if (!isThemeInited) {
+      if (!isThemeInited && defaultTheme) {
         setTheme(defaultTheme)
         setThemeInited(true)
       }
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,27 +1,20 @@
+export { initAuthData } from "./model/services/initAuthData";
 
-export { saveJsonSettings } from './model/services/saveJsonSettings';
+export { saveJsonSettings } from "./model/services/saveJsonSettings";
 
-export {
-  useJsonSettings,
-} from "./model/selectors/jsonSettings";
+export { useJsonSettings } from "./model/selectors/jsonSettings";
 
-export {
-    getUserAuthData,
-} from './model/selectors/getUserAuthData/getUserAuthData';
+export { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 
-export {
-    getUserInited,
-} from './model/selectors/getUserInited/getUserInited';
-
-export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
+export { getUserInited } from "./model/selectors/getUserInited/getUserInited";
 
 export {
-    userReducer,
-    userActions,
-} from './model/slice/userSlice';
+  isUserAdmin,
+  isUserManager,
+  getUserRoles,
+} from "./model/selectors/roleSelectors";
 
-export type {
-    UserSchema,
-    User,
-} from './model/types/user';
-export { UserRole } from './model/consts/userConsts';
+export { userReducer, userActions } from "./model/slice/userSlice";
+
+export type { UserSchema, User } from "./model/types/user";
+export { UserRole } from "./model/consts/userConsts";
